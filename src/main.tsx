c161d7795310c16d67ef712fec6912dbf90da773
deep-awak <mahery.razafimanantsoa.dev@gmail.com>
2026-09-18 10:49:24 +0300

    fix:  make 404 error

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-(function () {
-  const redirect = sessionStorage.redirect;
-  delete sessionStorage.redirect;
-  if (redirect && redirect !== location.href) {
-    history.replaceState(null, "", redirect);
-  }
-})();
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
